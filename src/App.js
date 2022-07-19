3bed18f3923b19ddf55f0d7b11bfd31038260465
anamarincic <amarincic5@gmail.com>
2022-07-19 21:04:05 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useUser } from "./contexts/UserContext";
+import { ChatPage } from "./pages/ChatPage";
+import { JoinPage } from "./pages/JoinPage/JoinPage.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { joined } = useUser();
+
+  return joined ? <ChatPage /> : <JoinPage />;
 }
 
 export default App;
